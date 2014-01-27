851972d2d3e9b830664ffc9132f7ccd68d1b22bb
Luke GB <git@lukegb.com>
2014-01-27 00:14:39 +0000

    Seriously

--- src/main/java/com/lukegb/jenkins/plugins/DlCenterPusher.java
+++ src/main/java/com/lukegb/jenkins/plugins/DlCenterPusher.java
@@ -221,13 +221,13 @@ public class DlCenterPusher extends Recorder {
                 
                 HttpResponse resp = httpClient.execute(target, addRequest);
                 HttpEntity returnEntity = resp.getEntity();
-                returnEntity.consumeContent();
                 if (resp.getStatusLine().getStatusCode() != 201) {
                     listener.getLogger().println("dlcenter ==> " + resp.getStatusLine());
                     returnEntity.writeTo(listener.getLogger());
                     ex = new IOException("Unexpected status code from dlcenter: got " + resp.getStatusLine());
                     continue;
                 } else {
+                    returnEntity.consumeContent();
                     ex = null;
                     listener.getLogger().println("dlcenter --> " + resp.getHeaders("Location")[0].getValue());
                     break;
